c4ac24d32fd1cc66fd20fc5095026320025557d7
Sipaman2901 <sipaman@gmail.com>
2025-06-20 17:05:31 +0300

    Correcting task 1

--- Lesson_03/User.py
+++ Lesson_03/User.py
@@ -1,17 +1,13 @@
 class User:
-    def __init__(self, first_name, middle_name, last_name):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
-        self.middle_name = middle_name
         self.last_name = last_name
 
     def print_first_name(self):
         print(self.first_name)
 
-    def print_middle_name(self):
-        print(self.middle_name)
-
     def print_last_name(self):
         print(self.last_name)
 
     def print_full_name(self):
-        print(f"{self.last_name} {self.first_name} {self.middle_name}")
+        print(f"{self.first_name} {self.last_name}")
